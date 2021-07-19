d0db1ada7309861efe68c60d182304c8cca65606
Rakesh Kumar <crazyrake003@gmail.com>
2021-07-20 01:22:00 +0530

    Added the ideology for adding

--- LinkGenerator.py
+++ LinkGenerator.py
@@ -11,6 +11,89 @@ Libraries Used:
 
 from tkinter import * 
 
+#>>>>>>>>>>>>>For Adding(List Creation)
+
+buffer_list=[]
+
+
+'''
+Add Function
+
+Creating a list of list which can save Link.get() and Topic.get()
+
+Add should contain:
+    Link and Topic should refresh
+    File Name Gui should hide <Hide function is executed here>
+    Last added link and topic name should be shown with a option to delete(optionally): <confirm_Safety function is executed here>
+        If pressed another function called Delete should get proceed further
+
+'''
+
+'''
+Reference for list creation:  
+    
+    outside_list=[]
+    for i in range(0,5):
+        inside_list=[]
+        inside_list.append(i)
+        inside_list.append(i+1)
+        outside_list.append(inside_list)
+
+       #you can access any inside_list from the outside_list and append     
+        outside_list[1].append(100)
+        print(outside_list)  '''
+
+def add(Linkk,Topicc):
+    
+    for i in range(0,1):
+        temp_list=[]
+        temp_list.append(Linkk)
+        temp_list.append(Topicc)
+        buffer_list.append(temp_list)
+    pass
+
+
+
+'''
+hide
+
+This function is a static function which has a constant duty of hiding the Title GUI if Add is pressed
+Can set a flag to enable and disable
+
+'''
+
+def hide():
+    pass
+
+
+
+'''
+confirm_Safety Function
+
+Creation of New GUI which shows previous Title, Topic values with a button named 'delete--->delete()-->is called' 
+The values are dynamic and taken from buffer_list accessed via index (may be..!)
+
+'''
+def confirm_Safety():
+    pass
+
+
+
+'''
+Delete Function
+
+This function is accompanied with confirm_Safety function . If Delete button is pressed,
+Now the corresponding List and Topic is deleted from the buffer list (buy deleting the corresponding index value)
+And call the confirm_Safety function with the previous Topic and Link value
+
+'''
+def delete():
+    pass
+
+
+
+
+
 
 
 #>>>>>>>>>>>>>>File Handling
@@ -33,6 +116,8 @@ def save_info():
     file.write("</body>")
     file.write("</html>")
     '''
+Reference:
+        
     file.write("Your First Name " + firstname_info)
     
     file.write("\n")
@@ -47,7 +132,9 @@ def save_info():
     
     '''
     
-'''    <html>
+''' 
+Trials:
+<html>
 <body>
 
 <a href="https://www.youtube.com/watch?v=XAwZwj5H9RE" target="_blank">Youtube<br></a>
